509814efbbcbf28ee145432df19770b3f10c04d2
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-07-31 13:47:56 -0500

    Update CardMonkey.cs

--- CardMonkey.cs
+++ CardMonkey.cs
@@ -13,34 +13,34 @@ namespace CardMonkey
     /// <summary>
     /// The main class that adds the core tower to the game
     /// </summary>
-    public class CardMonkey : ModTower
+    public class CGlueGunner : ModTower
     {
         // public override string Portrait => "Don't need to override this, using the default of Name-Portrait";
         // public override string Icon => "Don't need to override this, using the default of Name-Icon";
 
         public override TowerSet TowerSet => TowerSet.Primary;
 
-        public override string BaseTower => TowerType.DartMonkey;
+        public override string BaseTower => TowerType.GlueMonkey;
         public override int Cost => 400;
 
         public override int TopPathUpgrades => 5;
         public override int MiddlePathUpgrades => 5;
-        public override int BottomPathUpgrades => 5;
+        public override int BottomPathUpgrades => 4;
         public override string Description => "Throws playing cards at Bloons";
 
         // public override string DisplayName => "Don't need to override this, the default turns it into 'Card Monkey'"
 
-        public override ParagonMode ParagonMode => ParagonMode.Base555;
+        // public override ParagonMode ParagonMode => ParagonMode.Base555;
 
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
-            towerModel.range += 10;
-            var attackModel = towerModel.GetAttackModel();
-            attackModel.range += 10;
+            //towerModel.range += 10;
+            //var attackModel = towerModel.GetAttackModel();
+            //attackModel.range += 10;
 
-            var projectile = attackModel.weapons[0].projectile;
-            projectile.ApplyDisplay<RedCardDisplay>(); // Make the projectiles look like Cards
-            projectile.pierce += 2;
+            //var projectile = attackModel.weapons[0].projectile;
+            //projectile.ApplyDisplay<RedCardDisplay>(); // Make the projectiles look like Cards
+            //projectile.pierce += 2;
         }
 
         /// <summary>
@@ -48,10 +48,10 @@ namespace CardMonkey
         /// <br />
         /// If we didn't have this, it would just put it at the end of the Primary section
         /// </summary>
-        public override int GetTowerIndex(List<TowerDetailsModel> towerSet)
-        {
-            return towerSet.First(model => model.towerId == TowerType.BoomerangMonkey).towerIndex + 1;
-        }
+        //public override int GetTowerIndex(List<TowerDetailsModel> towerSet)
+        //{
+           // return towerSet.First(model => model.towerId == TowerType.BoomerangMonkey).towerIndex + 1;
+        //}
 
         /// <summary>
         /// Support the Ultimate Crosspathing Mod
