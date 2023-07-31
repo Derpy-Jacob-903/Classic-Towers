bf70dd5ab71fffcaf2acd2fb8debff95eb5c1eb8
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-07-31 13:43:25 -0500

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using BTD_Mod_Helper;
+using CardMonkey;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(CardMonkeyMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace CardMonkey
+{
+    public class CardMonkeyMod : BloonsTD6Mod
+    {
+        // No Harmony Patches or hooks required for this whole tower!
+    }
+}
